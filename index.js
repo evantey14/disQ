76c4e600bf604efbd01aeae1c122a57acb3993e1
Evan Tey <evantey14@gmail.com>
2016-08-02 08:14:51 -0500

    updated content

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('user connected');
+  
+  socket.on('enqueue', function(name){
+    io.emit('enqueue', name);
   });
+  
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
