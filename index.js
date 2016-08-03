f7ff4b77f25ed46ae516d877c18af11490d626b1
Evan Tey <evantey14@gmail.com>
2016-08-02 19:34:03 -0500

    allow q-ing

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ io.on('connection', function(socket){
   console.log('user connected');
   
   socket.on('enqueue', function(name){
-    io.emit('enqueue', name);
+    io.emit('enqueue', name, Math.floor((Math.random() * 1000) + 1));
+  });
+
+  socket.on('dequeue', function(){
+  	io.emit('dequeue');
   });
   
   socket.on('disconnect', function(){
