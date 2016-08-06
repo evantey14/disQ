38ddf31c8529d0402a5543e5b9ef333fa4144424
Evan Tey <evantey14@gmail.com>
2016-08-05 22:06:11 -0500

    record usernames

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(http);
 var qModel = require('./q');
 var q = null;
 var question = 'Question';
+var usernames = {};
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -20,10 +21,16 @@ io.on('connection', function(socket) {
     io.emit('updateQ', q.q);
     io.emit('updateQuestion', question);
   });
+
+  socket.on('addUser', function(username){
+    socket.username = username;
+    usernames[username] = username;
+  });
   
   socket.on('setQuestion', function(new_question){
     question = new_question;
     io.emit('updateQuestion', question);
+    console.log(socket.username);
   });
 
   socket.on('enqueue', function(name){
